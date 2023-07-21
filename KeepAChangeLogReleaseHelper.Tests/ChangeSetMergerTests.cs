f7863e18d946e4a92a187e0ffd81b8d4d61a43be
manuc66 <manuc66@gmail.com>
2023-07-21 21:51:12 +0200

    split test file

---
+++ KeepAChangeLogReleaseHelper.Tests/ChangeSetMergerTests.cs
@@ -0,0 +1,40 @@
+namespace KeepAChangeLogReleaseHelper.Tests;
+
+public class ChangeSetMergerTests
+{
+    [Test]
+    public void ItComputeAMajorFromChanged()
+    {
+        string[] changesets =
+        {
+            @"
+        ## Changed
+        - Improved existing feature 1.
+
+        ",
+            @" ## Fixed
+        - Bug fix 1.
+        - Bug fix 2.
+        ",
+            @"
+        ## Added
+        - New feature 1.
+        - New feature 2."
+        };
+
+        string mergedResult = ChangeSetMerger.Merge(changesets);
+
+        Assert.That(mergedResult, Is.EqualTo(
+            @"## Changed
+- Improved existing feature 1.
+
+## Added
+- New feature 1.
+- New feature 2.
+
+## Fixed
+- Bug fix 1.
+- Bug fix 2.
+"));
+    }
+}
