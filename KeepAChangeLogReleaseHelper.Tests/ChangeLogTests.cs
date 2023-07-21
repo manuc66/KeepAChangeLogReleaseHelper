da2a0ab3a774a390723e0e94b582702670eed132
manuc66 <manuc66@gmail.com>
2023-07-21 23:41:38 +0200

    It can get last version

--- KeepAChangeLogReleaseHelper.Tests/ChangeLogTests.cs
+++ KeepAChangeLogReleaseHelper.Tests/ChangeLogTests.cs
@@ -150,6 +150,77 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Counter-examples: ""What makes unicorns cry?"".
 "));
     }
+    [Test]
+    public void ItCanGetLastReleaseWhenUnlreseaseIsPresent()
+    {
+        ChangeLog changeLog = new ChangeLog(@"# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Changed
+- Improved existing feature qsdqsnldnk.
+
+### Added
+- New feature fdgdfg.
+
+### Secirity
+- Address CVS-2131314
+
+## [0.1.0] - 2014-05-31
+
+### Added
+
+- This CHANGELOG file to hopefully serve as an evolving example of a
+  standardized open source project CHANGELOG.
+- CNAME file to enable GitHub Pages custom domain.
+- README now contains answers to common questions about CHANGELOGs.
+- Good examples and basic guidelines, including proper date formatting.
+- Counter-examples: ""What makes unicorns cry?"".
+");
+        
+        Assert.That(changeLog.LastVersion, Is.EqualTo("0.1.0"));
+    }
+    [Test]
+    public void ItCanGetLastReleaseWhenUnlreseaseIsNotThere()
+    {
+        ChangeLog changeLog = new ChangeLog(@"# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.1.0] - 2014-05-31
+
+### Added
+
+- This CHANGELOG file to hopefully serve as an evolving example of a
+  standardized open source project CHANGELOG.
+- CNAME file to enable GitHub Pages custom domain.
+- README now contains answers to common questions about CHANGELOGs.
+- Good examples and basic guidelines, including proper date formatting.
+- Counter-examples: ""What makes unicorns cry?"".
+");
+        
+        Assert.That(changeLog.LastVersion, Is.EqualTo("0.1.0"));
+    }
+    [Test]
+    public void ItCanGetLastReleaseWhenUNoVersion()
+    {
+        ChangeLog changeLog = new ChangeLog(@"# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).");
+        
+        Assert.That(changeLog.LastVersion, Is.EqualTo("0.0.0"));
+    }
 
 
     [Test]
