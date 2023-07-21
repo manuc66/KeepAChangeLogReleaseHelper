2f34f9b266b48813205137109781554e319c21e2
manuc66 <manuc66@gmail.com>
2023-07-21 21:51:12 +0200

    split test file

--- KeepAChangeLogReleaseHelper.Tests/GetNextSemanticVersionTests.cs
+++ KeepAChangeLogReleaseHelper.Tests/GetNextSemanticVersionTests.cs
@@ -1,44 +1,5 @@
 namespace KeepAChangeLogReleaseHelper.Tests;
 
-public class ChangeSetMergerTests
-{
-    [Test]
-    public void ItComputeAMajorFromChanged()
-    {
-        string[] changesets =
-        {
-            @"
-        ## Changed
-        - Improved existing feature 1.
-
-        ",
-            @" ## Fixed
-        - Bug fix 1.
-        - Bug fix 2.
-        ",
-            @"
-        ## Added
-        - New feature 1.
-        - New feature 2."
-        };
-
-        string mergedResult = ChangeSetMerger.Merge(changesets);
-
-        Assert.That(mergedResult, Is.EqualTo(
-            @"## Changed
-- Improved existing feature 1.
-
-## Added
-- New feature 1.
-- New feature 2.
-
-## Fixed
-- Bug fix 1.
-- Bug fix 2.
-"));
-    }
-}
-
 public class GetNextSemanticVersionTests
 {
     [SetUp]
