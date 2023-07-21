b55ec2aacff6f62d858a4e7cc24141a3ca8e29dd
manuc66 <manuc66@gmail.com>
2023-07-21 23:36:46 +0200

    ItCanProduceARelease from a changelog sets

--- KeepAChangeLogReleaseHelper.Tests/ChangeLogTests.cs
+++ KeepAChangeLogReleaseHelper.Tests/ChangeLogTests.cs
@@ -71,6 +71,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Counter-examples: ""What makes unicorns cry?"".
 "));
     }
+
     [Test]
     public void ItCanUpdateTheUnreleasedSectionByReplacingExistingContent()
     {
@@ -138,6 +139,87 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Bug fix 2.
 
 ## [0.0.1] - 2014-05-31
+
+### Added
+
+- This CHANGELOG file to hopefully serve as an evolving example of a
+  standardized open source project CHANGELOG.
+- CNAME file to enable GitHub Pages custom domain.
+- README now contains answers to common questions about CHANGELOGs.
+- Good examples and basic guidelines, including proper date formatting.
+- Counter-examples: ""What makes unicorns cry?"".
+"));
+    }
+
+
+    [Test]
+    public void ItCanProduceARelease()
+    {
+        ChangeLog changeLog = new ChangeLog(@"# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Changed
+- Improved existing feature qsdqsnldnk.
+
+### Added
+- New feature fdgdfg.
+
+### Secirity
+- Address CVS-2131314
+
+## [0.1.0] - 2014-05-31
+
+### Added
+
+- This CHANGELOG file to hopefully serve as an evolving example of a
+  standardized open source project CHANGELOG.
+- CNAME file to enable GitHub Pages custom domain.
+- README now contains answers to common questions about CHANGELOGs.
+- Good examples and basic guidelines, including proper date formatting.
+- Counter-examples: ""What makes unicorns cry?"".
+").Release(new DateTime(2023, 7, 9, 12, 5, 2, 3, 11),@"
+        ## Changed
+        - Improved existing feature 1.
+
+        ",
+            @" ## Fixed
+        - Bug fix 1.
+        - Bug fix 2.
+        ",
+            @"
+        ## Added
+        - New feature 1.
+        - New feature 2."
+        );
+        
+        Assert.That(changeLog.LastVersion, Is.EqualTo("1.0.0"));
+        Assert.That(changeLog.ToString(), Is.EqualTo(@"# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.0.0] - 2023-07-09
+
+### Changed
+- Improved existing feature 1.
+
+### Added
+- New feature 1.
+- New feature 2.
+
+### Fixed
+- Bug fix 1.
+- Bug fix 2.
+
+## [0.1.0] - 2014-05-31
 
 ### Added
 
