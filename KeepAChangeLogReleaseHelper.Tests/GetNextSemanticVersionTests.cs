55c8e8aa96aab5548ac8527584c5f267d6553716
manuc66 <manuc66@gmail.com>
2023-07-21 12:18:39 +0200

    Finish implementing empty sections

--- KeepAChangeLogReleaseHelper.Tests/GetNextSemanticVersionTests.cs
+++ KeepAChangeLogReleaseHelper.Tests/GetNextSemanticVersionTests.cs
@@ -8,36 +8,6 @@ public class GetNextSemanticVersionTests
     }
 
     [Test]
-    public void ItComputeAMinorIfOnlyChangedIsEmpty()
-    {
-        string changeset = @"
-        ## Added
-        - New feature 1.
-        - New feature 2.
-
-        ## Changed
-
-        ## Deprecated
-        - Deprecated feature 1.
-
-        ## Removed
-        - Removed feature 2.
-
-        ## Fixed
-        - Bug fix 1.
-        - Bug fix 2.
-
-        ## Security
-        - Security update 1.
-        
-        ";
-
-        string nextVersion = GetNextSemanticVersion(changeset, "1.0.0");
-
-        Assert.That(nextVersion, Is.EqualTo("1.1.0"));
-    }
-
-    [Test]
     public void ItComputeAMajorFromChanged()
     {
         string changeset = @"
@@ -137,6 +107,112 @@ public class GetNextSemanticVersionTests
 
         Assert.That(nextVersion, Is.EqualTo("2.0.0"));
     }
+    
+
+    [Test]
+    public void ItComputeAMinorIfOnlyChangedIsEmpty()
+    {
+        string changeset = @"
+        ## Added
+        - New feature 1.
+        - New feature 2.
+
+        ## Changed
+
+        ## Deprecated
+        - Deprecated feature 1.
+
+        ## Removed
+        - Removed feature 2.
+
+        ## Fixed
+        - Bug fix 1.
+        - Bug fix 2.
+
+        ## Security
+        - Security update 1.
+        
+        ";
+
+        string nextVersion = GetNextSemanticVersion(changeset, "1.0.0");
+
+        Assert.That(nextVersion, Is.EqualTo("1.1.0"));
+    }
+
+    [Test]
+    public void ItComputeAFixIfOnlyFixedAndSecurityAreFilled()
+    {
+        string changeset = @"
+        ## Added
+
+        ## Changed
+
+        ## Deprecated
+
+        ## Removed
+
+        ## Fixed
+        - Bug fix 1.
+        - Bug fix 2.
+
+        ## Security
+        - Security update 1.
+        
+        ";
+
+        string nextVersion = GetNextSemanticVersion(changeset, "1.0.0");
+
+        Assert.That(nextVersion, Is.EqualTo("1.0.1"));
+    }
+
+    [Test]
+    public void ItComputeAPathIfOnlyFixedIsFilled()
+    {
+        string changeset = @"
+        ## Added
+
+        ## Changed
+
+        ## Deprecated
+
+        ## Removed
+
+        ## Fixed
+        - Bug fix 1.
+        - Bug fix 2.
+
+        ## Security
+        
+        ";
+
+        string nextVersion = GetNextSemanticVersion(changeset, "1.0.0");
+
+        Assert.That(nextVersion, Is.EqualTo("1.0.1"));
+    }
+
+    [Test]
+    public void ItComputeAPathIfOnlySecurityIsFilled()
+    {
+        string changeset = @"
+        ## Added
+
+        ## Changed
+
+        ## Deprecated
+
+        ## Removed
+
+        ## Fixed
+
+        ## Security
+        - Security update 1.
+        
+        ";
+
+        string nextVersion = GetNextSemanticVersion(changeset, "1.0.0");
+
+        Assert.That(nextVersion, Is.EqualTo("1.0.1"));
+    }
 
     static string GetNextSemanticVersion(string changeset, string currentVersion)
     {
@@ -144,34 +220,91 @@ public class GetNextSemanticVersionTests
         IEnumerable<string> lines = changeset.Split('\n').Select(line => line.Trim());
 
         bool major = false;
-        bool majorStarted = false;
+        bool minor = false;
+        bool patch = false;
+        bool changedStarted = false;
+        bool addedStarted = false;
+        bool removedStarted = false;
+        bool deprecatedStarted = false;
+        bool fixedStarted = false;
+        bool securityStarted = false;
         foreach (string line in lines)
         {
             if (line.StartsWith("## Changed", StringComparison.OrdinalIgnoreCase))
             {
-                majorStarted = true;
+                changedStarted = true;
+                addedStarted = false;
+                removedStarted = false;
+                deprecatedStarted = false;
+            }
+            else if (line.StartsWith("## Added", StringComparison.OrdinalIgnoreCase))
+            {
+                changedStarted = false;
+                addedStarted = true;
+                removedStarted = false;
+                deprecatedStarted = false;
+            }
+            else if (line.StartsWith("## Removed", StringComparison.OrdinalIgnoreCase))
+            {
+                changedStarted = false;
+                addedStarted = false;
+                removedStarted = true;
+                deprecatedStarted = false;
+            }
+            else if (line.StartsWith("## Deprecated", StringComparison.OrdinalIgnoreCase))
+            {
+                changedStarted = false;
+                addedStarted = false;
+                removedStarted = false;
+                deprecatedStarted = true;
+            }
+            else if (line.StartsWith("## Fixed", StringComparison.OrdinalIgnoreCase))
+            {
+                changedStarted = false;
+                addedStarted = false;
+                removedStarted = false;
+                deprecatedStarted = false;
+                fixedStarted = true;
+                securityStarted = false;
+            }
+            else if (line.StartsWith("## Security", StringComparison.OrdinalIgnoreCase))
+            {
+                changedStarted = false;
+                addedStarted = false;
+                removedStarted = false;
+                deprecatedStarted = false;
+                fixedStarted = false;
+                securityStarted = true;
             }
             else if (line.StartsWith("## ", StringComparison.OrdinalIgnoreCase))
             {
-                majorStarted = false;
+                changedStarted = false;
+                addedStarted = false;
+                removedStarted = false;
+                deprecatedStarted = false;
+                fixedStarted = false;
+                securityStarted = false;
             }
             else
             {
                 if (!string.IsNullOrWhiteSpace(line))
                 {
-                    if (majorStarted)
+                    if (changedStarted)
                     {
                         major = true;
+                    }
+                    if (addedStarted || removedStarted || deprecatedStarted)
+                    {
+                        minor = true;
+                    }
+                    if (fixedStarted || securityStarted)
+                    {
+                        patch = true;
                     }
                 }
             }
         }
-
-        // Check for changes in each category
-        bool minor = lines.Any(line => line.StartsWith("## Added", StringComparison.OrdinalIgnoreCase) || line.StartsWith("## Removed", StringComparison.OrdinalIgnoreCase) ||
-                                       line.StartsWith("## Deprecated", StringComparison.OrdinalIgnoreCase));
-        bool patch = lines.Any(line => line.StartsWith("## Fixed", StringComparison.OrdinalIgnoreCase) || line.StartsWith("## Security", StringComparison.OrdinalIgnoreCase));
-
+        
         return ComputeVersion(currentVersion, major, minor, patch);
     }
 
